0f089250d18046d31e260895acec240c7cf10920
yerim <sola4718@gmail.com>
2023-07-26 15:45:16 +0900

    상품 추가 서비스 작성

--- services/items.service.js
+++ services/items.service.js
@@ -24,6 +24,21 @@ class ItemsService {
   
     const CreateItemData = await this.itemRepository.createItem(name,optionId,price,type,amount);
 
+    if (!name) {
+      throw new Error('상품 이름을 입력해주세요');
+    }
+
+    // 가격이 없을 경우
+    if (!price) {
+      throw new Error('상품 가격을 입력해주세요');
+    }
+
+    // 알맞은 타입이 아닐 경우
+    const validTypes = ['Coffee', 'Beverage', 'Tea', 'Cake', 'Cookie'];
+    if (!validTypes.includes(type)) {
+      throw new Error('알맞은 타입을 지정해주세요');
+    }
+
     return {
       itemId: CreateItemData.itemId,
       name: CreateItemData.name,
