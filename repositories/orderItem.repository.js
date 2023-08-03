e6da09cc9f9dd463d9d91647d41f849fde6c85e7
yerim <sola4718@gmail.com>
2023-08-03 12:32:44 +0900

    트랜젝션 수정 완료

--- repositories/orderItem.repository.js
+++ repositories/orderItem.repository.js
@@ -40,56 +40,86 @@ class OrderItemRepository {
     const t = await sequelize.transaction({
       isolationLevel: Transaction.ISOLATION_LEVELS.READ_COMMITTED,
     });
-
+  
     try {
       const orderItem = await OrderItems.findOne({ where: { id: payload.orderItemId } });
       if (!orderItem) {
         throw new Error('주문 아이템이 존재하지 않습니다.');
       }
-
+  
       // 숫자로 입력된 상태 코드를 문자열로 변환
       const newState = orderItemState[payload.state];
       if (!newState) {
         throw new Error('유효하지 않은 상태 코드입니다.');
       }
-
+  
       // 'PENDING' → 'COMPLETED' 상태로 변경될 때
       if (payload.state === 1 && orderItem.state !== 2) {
         const updatedOrderItem = await OrderItems.update(
           { state: newState }, // 상태 코드를 문자열로 변경
           { where: { id: payload.orderItemId }, transaction: t }
         );
-
-
+  
         if (updatedOrderItem[0] === 0) {
           throw new Error('상품 주문에 실패했습니다.');
         }
-
+  
         const item = await Items.findByPk(payload.itemId, { transaction: t });
-
+  
         if (!item) {
           throw new Error('상품을 찾지 못했습니다.');
         }
-
+  
         const newAmount = item.amount + orderItem.amount;
         await Items.update(
           { amount: newAmount },
           { where: { id: payload.itemId }, transaction: t }
         );
-
+  
         await t.commit();
-
+  
         return {
           code: 200,
           message: '발주가 완료되었습니다.',
         };
       }
-
+  
+      // 발주완료된거 취소하거나 보류 주문상태로 되돌리기
+      if (payload.state === 2 && orderItem.state !== 2) {
+        const item = await Items.findByPk(payload.itemId, { transaction: t });
+  
+        if (!item) {
+          throw new Error('상품을 찾지 못했습니다.');
+        }
+  
+        if (item.amount < orderItem.amount) {
+          throw new Error('현재 수량이 발주 수량보다 적어 발주 취소가 불가능합니다.');
+        }
+  
+        const newAmount = item.amount - orderItem.amount;
+        await Items.update(
+          { amount: newAmount },
+          { where: { id: payload.itemId }, transaction: t }
+        );
+  
+        await OrderItems.update(
+          { state: newState }, // 상태 코드를 문자열로 변경
+          { where: { id: payload.orderItemId }, transaction: t }
+        );
+  
+        await t.commit();
+  
+        return {
+          code: 200,
+          message: '발주를 수정했습니다.',
+        };
+      }
+  
       // 다른 경우는 발주상태만 변경
-      await OrderItems.update({ state: payload.state }, { where: { id: payload.orderItemId }, transaction: t });
-
+      await OrderItems.update({ state: newState }, { where: { id: payload.orderItemId }, transaction: t });
+  
       await t.commit();
-
+  
       return {
         code: 200,
         message: '발주 상태가 변경되었습니다.',
